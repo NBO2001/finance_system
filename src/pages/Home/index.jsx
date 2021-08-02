2638deb311bb1566ec553f0084cb52debb57de2b
NBO2001 <natanbezerr63@gmail.com>
2021-08-02 08:17:34 -0400

    Implementacao da page home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,27 @@
+
+const Home = () => {
+
+    return(
+        <div>
+            <div>
+                <p>Month: August</p>
+                <div>
+                    <p>Gastos: 745.45</p>
+                    <p>Receita: 2548.59</p>
+                    <p>Investimentos: 558.45</p>
+                </div>
+            </div>
+
+            <div>
+                <p>Year: 2021</p>
+                <div>
+                    <p>Gastos: 4548.48</p>
+                    <p>Receita: 8545.54</p>
+                    <p>Investimentos: 4144.45</p> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
